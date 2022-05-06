e46925c9e9ac94cb1fd4ddbe43bd1d1f2e466601
AlanHoltz <alanholtz1999@gmail.com>
2022-05-06 18:12:13 -0300

    Primer Commit

---
+++ Consola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Utils;
+
+namespace Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            Console.WriteLine(Singleton.GetInstance().connectDB());
+        }
+    }
+}
---
+++ Utils/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Utils
+{
+    public sealed class Singleton
+    {
+        private static Singleton conn = null;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+
+
+            if (conn == null)
+            {
+                conn = new Singleton();
+            }
+            return conn;
+        }
+
+        public string connectDB()
+        {
+            SqlConnection conn;
+            string connString = "Data Source=server;Initial Catalog=db;User ID=user;Password=password";
+            try
+            {
+                conn = new SqlConnection(connString);
+                conn.Open();
+                Console.WriteLine("Connection Open ! ");
+                return connString;
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
