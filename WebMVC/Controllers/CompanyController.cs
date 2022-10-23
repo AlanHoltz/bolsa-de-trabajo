4942efe9d0e1730f5bc35d971bbd0d2a0bb0c945
Rocco <roccobosio@gmail.com>
2022-10-23 17:11:58 -0300

    List applicants

--- WebMVC/Controllers/CompanyController.cs
+++ WebMVC/Controllers/CompanyController.cs
@@ -1,17 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BolsaTrabajo.Models.Db;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebMVC.Models;
 
 namespace WebMVC.Controllers
 {
     public class CompanyController : Controller
     {
-        public IActionResult Index()
+        private readonly BolsaTrabajoContext _context;
+
+        public CompanyController(BolsaTrabajoContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult AddJobProfile()
+        public IActionResult Index()
         {
-            return View();
+            if (HttpContext.Session.GetString("Id") != null && HttpContext.Session.GetString("Type") == "Company")
+            {
+                return View();
+            }
+
+            return Redirect("/");
+        }
+
+        public IActionResult Proposals()
+        {
+            if(HttpContext.Session.GetString("Id") != null && HttpContext.Session.GetString("Type") == "Company")
+            {
+                int companyId = int.Parse(HttpContext.Session.GetString("Id"));
+
+                List<JobProfile> jobProfiles = _context.JobProfiles
+                                                .Include(jp => jp.Company)
+                                                .Where(jp => jp.StartingDate <= DateTime.Now 
+                                                && jp.EndingDate >= DateTime.Now 
+                                                && jp.CompanyId == companyId)
+                                                .ToList();
+            
+                return View(jobProfiles);
+            }
+
+            return Redirect("/");
+        }
+        
+        public IActionResult Proposal(int id)
+        {
+            if(HttpContext.Session.GetString("Id") != null && HttpContext.Session.GetString("Type") == "Company")
+            {
+                int companyId = int.Parse(HttpContext.Session.GetString("Id"));
+
+                List<JobProfilePerson> jobProfile = _context.JobProfilePerson
+                                            .Include(jpp => jpp.JobProfiles)
+                                            .Include(jpp => jpp.Persons)
+                                            .Include(jpp => jpp.Persons.User)
+                                            .Where(jp => jp.JobProfilesId == id 
+                                            && jp.JobProfiles.CompanyId == companyId)
+                                            .ToList();
+
+                ViewBag.quantity = jobProfile.Count();
+            
+                return View(jobProfile);
+            }
+
+            return Redirect("/");
         }
     }
 }
