f896d323749ab3d39603af4f0508e8f803bd5cb8
Rocco <roccobosio@gmail.com>
2022-10-31 22:55:42 -0300

    Company crud

--- WebMVC/Controllers/CompanyController.cs
+++ WebMVC/Controllers/CompanyController.cs
@@ -21,7 +21,16 @@ namespace WebMVC.Controllers
         public IActionResult Index()
         {
             if (HttpContext.Session.GetString("Id") == null) return Redirect("/login");
-            if (HttpContext.Session.GetString("IsAdmin") == "True") return View();
+            if (HttpContext.Session.GetString("IsAdmin") == "True")
+            {
+                List<Company> company = _context.Companies
+                                        .Include(company => company.City)
+                                        .Include(company => company.User)
+                                        .Where(company => company.User.Status == true)
+                                        .ToList();
+                
+                return View(company);
+            }
             if (HttpContext.Session.GetString("Type") == "Person") return Redirect("/Person");
 
             return Redirect("/Company/Proposals");
@@ -69,20 +78,106 @@ namespace WebMVC.Controllers
 
         public IActionResult Signup()
         {
-            return View();
+            if (HttpContext.Session.GetString("IsAdmin") == "True")
+            {
+                ViewBag.cities = _context.Cities.ToList();
+                return View();
+            }
+
+            return Redirect("/");
         }
 
         [HttpPost]
         public IActionResult Signup(Models.Company company)
         {
-            if (HttpContext.Session.GetString("IsAdmin") == "true")
+            if (HttpContext.Session.GetString("IsAdmin") == "True")
             {
                 company.Status = "Pending";
 
+                Models.User user = new User();
+                user.Mail = company.Mail;
+                user.Password = company.Password;
+                user.SignupDate = DateTime.Now;
+                user.Type = "Company";
+                user.Status = true;
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                company.Id = user.Id;
                 _context.Companies.Add(company);
                 _context.SaveChanges();
 
+                return Redirect("/Company");
+            }
+
+            return Redirect("/");
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            if (HttpContext.Session.GetString("Type") == "Company" || HttpContext.Session.GetString("IsAdmin") == "True")
+            {
+                Models.Company company = _context.Companies.Include(company => company.User).Where(company => company.Id == id).FirstOrDefault();
+
+                if (company != null)
+                {
+                    company.Mail = company.User.Mail;
+                    company.Password = company.User.Password;
+                    ViewBag.cities = _context.Cities.ToList();
+
+                    return View(company);
+                }
+
+                return Redirect("/Company");
+            }
+            else
+            {
                 return Redirect("/");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Models.Company company)
+        {
+            if (HttpContext.Session.GetString("Type") == "Company" || HttpContext.Session.GetString("IsAdmin") == "True")
+            {
+                Models.User user = new User();
+                user.Id = company.Id;
+                user.Mail = company.Mail;
+                user.Password = company.Password;
+                user.Type = "Company";
+                user.Status = true;
+
+                _context.Users.Update(user);
+                _context.SaveChanges();
+
+                _context.Companies.Update(company);
+                _context.SaveChanges();
+
+                return Redirect("/Company");
+            }
+
+            return Redirect("/");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (HttpContext.Session.GetString("IsAdmin") == "True")
+            {
+                Models.User company = _context.Users.Where(user => user.Id == id).FirstOrDefault();
+
+                if (company != null)
+                {
+                    company.Status = false;
+
+                    _context.Update(company);
+
+                    _context.SaveChanges();
+                }
+
+                return Redirect("/Company");
             }
 
             return Redirect("/");
--- WebMVC/Models/Company.cs
+++ WebMVC/Models/Company.cs
@@ -8,17 +8,32 @@ namespace WebMVC.Models
         [Key]
         [ForeignKey("User")]
         public int Id { get; set; }
+        [Required(ErrorMessage = "Email requerido")]
+        [EmailAddress(ErrorMessage = "Ingrese un email válido")]
+        [NotMapped]
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Contraseña requerida")]
+        [Display(Name = "Contraseña")]
+        [NotMapped]
+        public string Password { get; set; }
         [Required(ErrorMessage = "Nombre requerido")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
         [Required(ErrorMessage = "CUIT requerido")]
         public string Cuit { get; set; }
         [Required(ErrorMessage = "Dirección requerida")]
+        [Display(Name = "Dirección")]
         public string Address { get; set; }
         public string Photo { get; set; }
         [Required]
+        [Display(Name = "Ciudad")]
         public string CityZipCode { get; set; }
+        [Display(Name = "Nombre de referencia")]
         public string ReferenceName { get; set; }
+        [Display(Name = "Teléfono de referencia")]
         public string ReferencePhone { get; set; }
+        [EmailAddress(ErrorMessage = "Ingrese un email válido")]
+        [Display(Name = "Email de referencia")]
         public string ReferenceEmail { get; set; }
         public string ReferenceArea { get; set; }
         public string ReferenceWorkingOnCompany { get; set; }
